5f2b369b126081d32d332f8ef41d20b13aaa54c2
Willian Marcos <will_up28@hotmail.com>
2020-10-13 23:34:36 -0300

    Adicionando mapa à aplicação

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,24 @@
+const map = L.map("mapid").setView([-23.6686047,-46.6175243], 16)
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+}) 
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar pequeno Leão <a href="orphans.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+
+L.marker([-23.6686047,-46.6175243], { icon })
+  .addTo(map)
+  .bindPopup(popup)
+  
