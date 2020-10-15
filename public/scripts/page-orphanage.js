1be9884a7a01240813ba4d1cc22e36314c96f6df
Willian Marcos <will_up28@hotmail.com>
2020-10-15 14:51:05 -0300

    Adicinando imagens aleatórias com Javascript

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -21,4 +21,27 @@ const icon = L.icon({
 L.marker([-23.6686047,-46.6175243], { icon })
   .addTo(map)
  
+  /* image gallery */
+
+  function selectImage(event) {
+      const button = event.currentTarget
+
+      // remover todas as classes .active
+      const buttons = document.querySelectorAll(".images button")
+      buttons.forEach(removeActiveClass)
+
+      function removeActiveClass(button) {
+          button.classList.remove("active")
+      }
+
+      // selecionar a image clicada
+      const image = button.children[0]
+      const imageContainer = document.querySelector(".orphanage-details > img")
+
+      // atualizar o container de image
+      imageContainer.src = image.src
+
+      // adicionar a classe .active para este botão  
+      button.classList.add('active')
+  }
   
